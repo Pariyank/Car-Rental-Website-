cd24ef14a9b373c603a671ba7452eb24f70a4ea8
Pariyank Sikarwar <116998021+Pariyank@users.noreply.github.com>
2022-12-07 20:19:45 +0530

    Add files via upload

---
+++ javascr.js
@@ -0,0 +1,18 @@
+
+const sr = ScrollReveal ({
+    distance:'60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal('.text',{delay: 200, origin: 'top'})
+sr.reveal('.form-container form',{delay: 800, origin: 'left'})
+sr.reveal('.heading',{delay: 800, origin: 'top'})
+sr.reveal('.ride-container .box',{delay: 600, origin: 'top'})
+sr.reveal('.services-container .box',{delay: 600, origin: 'top'})
+sr.reveal('.servicesss-container .box',{delay: 600, origin: 'top'})
+sr.reveal('.about-container',{delay: 600, origin: 'top'})
+sr.reveal('.reviews-container',{delay: 600, origin: 'top'})
+sr.reveal('.newsletter .box',{delay: 400, origin: 'bottom'});
+sr.reveal('.premium-container .box',{delay: 600,origin: 'top'})
